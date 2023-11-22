cea7824ca667943e4fa6b212b3017ca3b0b28730
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-11-22 14:12:40 +0000

    AirBnB_clone

--- console.py
+++ console.py
@@ -33,7 +33,7 @@ class HBNBCommand(cmd.Cmd):
     def preloop(self):
         """Prints if isatty is false"""
         if not sys.__stdin__.isatty():
-            print('(hbnb)')
+            print('(hbnb) ', end='')
 
     def precmd(self, line):
         """Reformat command line for advanced command syntax.
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -116,12 +116,38 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """ Create an object of any class"""
         if not args:
-            print("** class name missing **")
+            print("** arguments missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        # Extracting the class name and parameters
+        args_list = args.split(' ')
+        class_name = args_list[0]
+        args_list = args_list[1:]
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        # Parsing and creating a dictionary of parameters
+        parameters = {}
+        for arg in args_list:
+            key, value = arg.split('=')
+
+            # Handling string values
+            if value.startswith('"') and value.endswith('"'):
+                # Escaping double quotes in the string value
+                value = value[1:-1].replace('"', r'\"').replace('_', ' ')
+            else:
+                # Handling float and integer values
+                if '.' in value:
+                    value = float(value)
+                else:
+                    value = int(value)
+
+            parameters[key] = value
+
+        # Creating the instance with the parameters
+        new_instance = HBNBCommand.classes[class_name](**parameters)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -213,7 +239,7 @@ class HBNBCommand(cmd.Cmd):
             for k, v in storage._FileStorage__objects.items():
                 print_list.append(str(v))
 
-        print(print_list)
+        print('[{}]'.format(', '.join(print_list)))
 
     def help_all(self):
         """ Help information for the all command """
@@ -272,7 +298,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +306,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +345,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -7,25 +7,37 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """ Initializes the attributes. """
+        self.id = str(uuid.uuid4())
+
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if 'updated_at' in kwargs and 'created_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+                self.__dict__.update(kwargs)
+            else:
+                # Handle missing 'updated_at' or 'created_at' keys
+                self.created_at = kwargs.get('created_at', datetime.now())
+                self.updated_at = kwargs.get('updated_at', datetime.now())
+
+        # If it's a new instance, add a call to the new method on storage
+        from models import storage
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        obj_dict = {
+            k: v for k, v in self.__dict__.items() if k in self.__dict__
+            and v != ''
+        }
+        cls = type(self).__name__
+        return '[{}] ({}) {}'.format(cls, self.id, obj_dict)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
