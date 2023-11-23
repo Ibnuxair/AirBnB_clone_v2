b14b9532ececb2421e3a021ef1d7e2e660977b07
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-11-23 15:54:45 +0000

    AirBnB_clone

--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Place Module for HBNB project"""
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
