ab384a451f0247fd94181a57b8eff1db0c15c5fc
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-11-23 05:05:24 +0000

    AirBnB_clone

--- models/state.py
+++ models/state.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = kwargs.get('name', '')
+    name = Column(String(128), nullable=False)
+
+    # For DBStorage
+    if storage_type == 'db':
+        cities = relationship(
+            'City', cascade='all, delete-orphan', backref='state')
+    # For FileStorage
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute cities """
+            from models import storage
+            city_list = []
+            for city in storage.all('City').values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+                    return city_list
