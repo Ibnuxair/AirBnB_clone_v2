eea9a0dde283d4da8ca31dfce477c5e17b5ecda4
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2023-11-23 05:05:24 +0000

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """ Initializes the attributes. """
         self.id = str(uuid.uuid4())
 
         if not kwargs:
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
         else:
-            if 'updated_at' in kwargs and 'created_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                del kwargs['__class__']
-                self.__dict__.update(kwargs)
-            else:
-                # Handle missing 'updated_at' or 'created_at' keys
-                self.created_at = kwargs.get('created_at', datetime.now())
-                self.updated_at = kwargs.get('updated_at', datetime.now())
+            for key, value in kwargs.items():
+                if not hasattr(self, key):
+                    setattr(self, key, value)
 
-        # If it's a new instance, add a call to the new method on storage
-        from models import storage
-        storage.new(self)
+            # Set defaults if 'updated_at' and 'created_at' are missing
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.utcnow()
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.utcnow()
+
+            # Convert 'updated_at' and 'created_at' strings to datetime
+            if isinstance(self.created_at, str):
+                self.created_at = datetime.strptime(
+                    self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            if isinstance(self.updated_at, str):
+                self.updated_at = datetime.strptime(
+                    self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -42,7 +52,8 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        storage.new(self)
+        self.updated_at = datetime.utcnow()
         storage.save()
 
     def to_dict(self):
@@ -53,4 +64,14 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        # Remove _sa_instance_state if it exists
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
+
         return dictionary
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
--- models/place.py
+++ models/place.py
@@ -1,32 +1,57 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+"""Place Module for HBNB project"""
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey(
+        'places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey(
+        'amenities.id'), primary_key=True, nullable=False)
+)
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """The Place class"""
+    __tablename__ = 'places'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.city_id = kwargs.get('city_id', '')
-        self.user_id = kwargs.get('user_id', '')
-        self.name = kwargs.get('name', '')
-        self.description = kwargs.get('description', '')
-        self.number_rooms = kwargs.get('number_rooms', '')
-        self.number_bathrooms = kwargs.get('number_bathrooms', '')
-        self.max_guest = kwargs.get('max_guest', '')
-        self.price_by_night = kwargs.get('price_by_night', '')
-        self.latitude = kwargs.get('latitude', '')
-        self.longitude = kwargs.get('longitude', '')
-        self.amenity_ids = kwargs.get('amenity_ids', '')
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    reviews = relationship('Review', cascade='all, delete', backref='place')
+    amenities = relationship(
+        "Amenity", secondary=place_amenity, viewonly=False
+    )
+
+    @property
+    def reviews(self):
+        from models import storage
+        review_list = []
+        for review in storage.all(Review).values():
+            if review.place_id == self.id:
+                review_list.append(review)
+                return review_list
+
+    @property
+    def amenities(self):
+        from models import storage
+        amenity_list = []
+        for amenity_id in self.amenity_ids:
+            amenity = storage.get(Amenity, amenity_id)
+            if amenity:
+                amenity_list.append(amenity)
+        return amenity_list
+
+    @amenities.setter
+    def amenities(self, obj):
+        if isinstance(obj, Amenity):
+            self.amenity_ids.append(obj.id)
