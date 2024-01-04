64ccf57bd0f04dde3cbde4a793c7abe1f9922023
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-01-04 15:44:03 +0000

    Task10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,33 +3,34 @@
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Close SQLAlchemy Session after each request."""
+def close_storage(exception):
+    """Close the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
-def states():
-    states = storage.all(State).values()
+def display_states():
+    """Display a HTML page with a list of all State objects"""
+    states = storage.all('State').values()
     sorted_states = sorted(states, key=lambda state: state.name)
-
-    return render_template('states.html', states=sorted_states)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def state_cities(id):
-    state = storage.get(State, id)
-    if state:
-        return render_template('state.html', state=state)
-    else:
-        return render_template('not_found.html')
+    return render_template('9-states.html', states=sorted_states)
 
 
-if __name__ == "__main__":
+@app.route('/states/<state_id>', strict_slashes=False)
+def display_cities_by_state(state_id):
+    """Display a HTML page with a list of City objects linked to a State"""
+    states = storage.all('State').values()
+    for state in states:
+        if state.id == state_id:
+            cities = sorted(state.cities, key=lambda city: city.name)
+            return render_template('9-cities.html', state=state, cities=cities)
+    return render_template('9-not_found.html')
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
